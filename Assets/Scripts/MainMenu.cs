b4558532df6b3fcba21fd11f1fa0239283581d45
Sanjit Singh <sanjitpsingh@yahoo.com>
2019-03-17 01:31:47 -0400

    Made fixes to Buildplayer. Scenes can now be loaded through Mainmenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	//Organ Model Scene
+    public void Organs()
+    {
+        SceneManager.LoadScene("OrganModels");
+    }
+
+    //Room Scene
+    public void Room()
+    {
+        SceneManager.LoadScene("Room");
+    }
+
+    //Test Scene
+    public void Test()
+    {
+        SceneManager.LoadScene("testscene");
+    }
+
+    //3D Scene
+    public void ThreeD()
+    {
+        SceneManager.LoadScene("threedee");
+    }
 }
