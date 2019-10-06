4e4d618ed89fb7971ac26ca371e76f74ccf67859
Sanjit Singh <sanjitpsingh@yahoo.com>
2019-10-05 21:09:33 -0400

    Added room scene, Fixed missing script errors, gameobject prefabs and Camera Display

---
+++ Assets/Scripts/MoveOnVoiceCommand.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+using UnityEngine.SceneManagement;
+
+
+public class MoveOnVoiceCommand : MonoBehaviour
+{
+
+    [SerializeField]
+    private Rigidbody cannonballPrefab;
+    [SerializeField]
+    private Transform firePoint;
+
+    private Dictionary<string, Action> keywordActions = new Dictionary<string, Action>();
+    private KeywordRecognizer keywordRecognizer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+		SceneManager.LoadScene ("testscene");
+		Debug.Log ("shut up");
+		SceneManager.LoadScene ("threedee");
+		Debug.Log ("i changed scenes");
+
+        keywordActions.Add("turn right", TurnRight);
+        keywordActions.Add("turn left", TurnLeft);
+        keywordActions.Add("move up", MoveUp);
+        //keywordActions.Add("move down", MoveDown);
+        //keywordActions.Add("move left", MoveLeft);
+        //keywordActions.Add("move right", MoveRight);
+
+        keywordRecognizer = new KeywordRecognizer(keywordActions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += OnKeywordsRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void OnKeywordsRecognized(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log("Keyword: " + args.text);
+        keywordActions[args.text].Invoke();
+    }
+
+    private void TurnLeft()
+    {
+        transform.Rotate(0f, -30f, 0f);
+    }
+
+    private void TurnRight()
+    {
+        transform.Rotate(0f, 30f, 0f);
+    }
+
+    private void MoveUp()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
