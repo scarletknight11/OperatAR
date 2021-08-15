6e9210565a315a92edc335fac1cf4ac6e7d241be
Sanjit Singh <ss3448@scarletmail.rutgers.edu>
2021-08-15 02:04:30 -0400

    reorganized files and reimported vuforia

--- Library/PackageCache/com.unity.xr.magicleap@3.0.0-preview.12/Runtime/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-using UnityEngine.Scripting;
-
-[assembly:AlwaysLinkAssembly]
-[assembly:InternalsVisibleTo("LuminUnity")]
-[assembly:InternalsVisibleTo("Unity.XR.MagicLeap.Tests.Editor")]
-[assembly:InternalsVisibleTo("UnityEditor.XR.MagicLeap")]
--- README.md
+++
@@ -1,4 +0,0 @@
-# OperatAR
-
-App that helps users learn how to perform surgery on a pretend patient and how to give directions in an operation room.
-Targeted specifcally for future medical students who are aspiring to become suregons of the future
